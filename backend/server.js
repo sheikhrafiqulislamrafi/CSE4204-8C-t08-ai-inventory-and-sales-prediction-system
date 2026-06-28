32e53c92da1a018d5e230e0f093e8a424f642046
antor77way <antorsadhu133634@gmail.com>
2026-06-29 01:49:28 +0600

    Configured Express server

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,7 @@
-const express = require("express");
+const app = require("./src/app");
+require("dotenv").config();
 
-const app = express();
-
-const PORT = 5000;
-
-app.get("/", (req, res) => {
-    res.send("Backend is running successfully!");
-});
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
